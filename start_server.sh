71520dcb16523348e36216ebb57947d4a6509874
vietngco <vietnguyencong99@gmail.com>
2023-12-24 17:53:37 -0600

    api & ui folders organize

--- start_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start the API server
-python3 -m llama_cpp.server --model "$MODEL_PATH" --chat_format "$CHAT_FORMAT" --n_gpu_layers "$GPU_LAYERS" &
-
-# Start the UI server
-python3 -m http.server 8080
