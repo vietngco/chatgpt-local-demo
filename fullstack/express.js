4182d9be12a2dd6dc378fbd4f64550d06e07fd6d
vietngco <vietnguyencong99@gmail.com>
2024-01-08 11:22:07 -0600

    Update server port in express.js

--- fullstack/express.js
+++ fullstack/express.js
@@ -35,5 +35,5 @@ app.post('/ai/chat/completions', function (req, res) {
 });
 
 app.listen(3000, function () {
-    console.log('Server is running on port 5000');
+    console.log('Server is running on port 3000');
 });
